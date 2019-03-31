f4b7d72f8ea28669395495f648c828ec10b71a68
chikaosike <46687784+chikaosike@users.noreply.github.com>
2019-03-30 22:41:40 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -17,18 +17,23 @@ function init() {
 
 
             var imageString = "";
-            imageString += "<img src='https://picsum.photos/g/600/400?image=" + picsum[1].id + "'>"
+            imageString += "<img src='https://picsum.photos/g/600/400?image=" + picsum[1].id + "'>" + "<br>" + picsum[1].author + "<br>" ;
+
+
             for (var x = 100; x < 175; x += 25) {
-                imageString += "<img src='https://picsum.photos/200/300?image=" + picsum[x].id + "'>"
-            }
-            for (var x = 175; x < 250; x += 25) {
-                imageString += "<img src='https://picsum.photos/g/200/300?image=" + picsum[x].id + "'>"
-            }
-            for (var x = 250; x < 325; x += 25) {
-                imageString += "<img src='https://picsum.photos/200/300?image=" + picsum[x].id + "'>"
+                imageString += "<img src='https://picsum.photos/200/300?image=" + picsum[x].id + "'>" + "<p>" + picsum[x].author + "</p>";
             }
 
-            // imageString += "<img src='https://picsum.photos/400/600?image=" + picsum[1].id + "'>"
+
+            for (var x = 175; x < 250; x += 25) {
+                imageString += "<img src='https://picsum.photos/g/200/300?image=" + picsum[x].id + "'>" + "<p>" + picsum[x].author + "</p>";
+            }
+
+
+            for (var x = 250; x < 325; x += 25) {
+                imageString += "<img src='https://picsum.photos/200/300?image=" + picsum[x].id + "'>" + "<p>" + picsum[x].author + "</p>";
+            }
+
 
             document.getElementById("gallery").innerHTML = imageString;
         }
